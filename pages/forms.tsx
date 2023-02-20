df6a58f6f7ea8140dc1aff40c21c7ce026fd54e2
topcircle <topcircle@gmail.com>
2023-02-20 17:17:02 +0900

    7.1 MAkring FOrms Alone

---
+++ pages/forms.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+export default function Forms() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [formErrors, setFormErrors] = useState("");
+  const [emailErrors, setEmailErrors] = useState("");
+
+  const onUsernameChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    setUsername(event.currentTarget.value);
+  };
+  const onEmailChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onPasswordChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const onSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (username === "" || email === "" || password === "") {
+      setFormErrors("all Fields are required");
+    }
+
+    console.log(username, email, password);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={username}
+        onChange={onUsernameChange}
+        type="text"
+        placeholder="Username"
+        required
+        minLength={5}
+      ></input>
+      <input
+        value={email}
+        onChange={onEmailChange}
+        type="email"
+        placeholder="Email"
+        required
+      ></input>
+      <input
+        value={password}
+        onChange={onPasswordChange}
+        type="password"
+        placeholder="Password"
+        required
+      ></input>
+      <input type="submit" value="Create Account" />
+    </form>
+  );
+}
