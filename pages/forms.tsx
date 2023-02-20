d97f6a162e52118073ccdf996a4fa44cd4153e6d
topcircle <topcircle@gmail.com>
2023-02-20 18:11:38 +0900

    7.4 validationm invalid

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,30 +1,44 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { FieldErrors } from "react-hook-form/dist/types";
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
 
 export default function Forms() {
-  const { register, handleSubmit } = useForm();
-  const onValid = () => {
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const onValid = (data: LoginForm) => {
     console.log("I'm valid");
   };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
+
   return (
-    <form onSubmit={handleSubmit(onValid)}>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
-        {...register("username", { required: true })}
+        {...register("username", {
+          required: "Username is required",
+          minLength: {
+            message: "the Uesrname shold be longer than 5 chars",
+            value: 5,
+          },
+        })}
         type="text"
         placeholder="Username"
-        required
       ></input>
       <input
-        {...register("email", { required: true })}
+        {...register("email", { required: "Email is required" })}
         type="email"
         placeholder="Email"
-        required
       ></input>
       <input
         {...register("password", { required: true })}
         type="password"
         placeholder="Password"
-        required
       ></input>
       <input type="submit" value="Create Account" />
     </form>
