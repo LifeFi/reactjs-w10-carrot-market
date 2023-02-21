cff85bc86a3d55533b2c18caa43b08e0d8709c0f
topcircle <topcircle@gmail.com>
2023-02-21 16:22:06 +0900

    10.1 protected handlers

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -5,14 +5,25 @@ export interface ResponseType {
   [key: string]: any;
 }
 
-export default function withHandler(
-  method: "GET" | "POST" | "DELETE",
-  fn: (req: NextApiRequest, res: NextApiResponse) => void
-) {
+interface ConfigType {
+  method: "GET" | "POST" | "DELETE";
+  fn: (req: NextApiRequest, res: NextApiResponse) => void;
+  isPrivate?: boolean;
+}
+
+export default function withHandler({
+  method,
+  fn,
+  isPrivate = true,
+}: ConfigType) {
   return async function (req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== method) {
       return res.status(405).end(); // 405 method not allowed
     }
+    if (isPrivate && !req.session.user) {
+      return res.status(401).json({ ok: false, error: "Plz log in" });
+    }
+
     try {
       await fn(req, res);
     } catch (error) {
