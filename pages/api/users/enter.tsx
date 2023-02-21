97f3aa88d6d4b8c178c1ef78942152aac87de370
topcircle <topcircle@gmail.com>
2023-02-21 12:12:15 +0900

    9.5 Sending Mail

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,9 +1,11 @@
+import mail from "@sendgrid/mail";
 import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+mail.setApiKey(process.env.SENDGRID_KEY!);
 
 async function handler(
   req: NextApiRequest,
@@ -38,6 +40,15 @@ async function handler(
       body: `Your login token is ${payload}`,
     });
     console.log(message);
+  } else if (email) {
+    const email = await mail.send({
+      from: "topcircle@gmail.com",
+      to: "topcircle@gmail.com",
+      subject: " Your Carrot Market Verification Email",
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+    console.log(email);
   }
 
   console.log(token);
