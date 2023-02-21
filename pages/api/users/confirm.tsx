4ffa12889860387ef64d4f15c1207e26c5fce9fe
topcircle <topcircle@gmail.com>
2023-02-21 13:20:33 +0900

    9.7 severless sessions

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,4 +1,6 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
+
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 
@@ -6,9 +8,24 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
+  console.log(req.session);
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: { payload: token },
+    // include: { user: true }, // 연결된 user 정보 가져오려면
+  });
+  if (!exists) res.status(404).end();
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
+  console.log(exists);
+
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password:
+    "991023091823098123908132901982asdasdasdasd12asdasda123123121231asdasd123",
+});
