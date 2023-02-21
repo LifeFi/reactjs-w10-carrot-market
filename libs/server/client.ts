cf111f4f558a4e73f14399e9bda852be7b5c0d48
topcircle <topcircle@gmail.com>
2023-02-21 18:25:41 +0900

    11.0 Product Model

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -1,3 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-export default new PrismaClient();
+declare global {
+  var client: PrismaClient | undefined;
+} // interface 사용도 괜찮다고 함.
+
+const client = global.client || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") global.client = client;
+
+export default client;
