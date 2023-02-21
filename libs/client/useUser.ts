1b20ff8cd3f1d438d9c2f182de46008ce856a951
topcircle <topcircle@gmail.com>
2023-02-21 17:51:25 +0900

    10.4 useUser refoctor

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,18 +1,14 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 export default function useUser() {
-  const [user, setUser] = useState();
+  const { data, error } = useSWR("/api/users/me");
   const router = useRouter();
   useEffect(() => {
-    fetch("/api/users/me")
-      .then((res) => res.json())
-      .then((data) => {
-        if (!data.ok) {
-          return router.replace("/enter"); // 뒤로가기를 못하게 하려면 replace 사용하는 것이 좋다. 히스토리에 안남음.
-        }
-        setUser(data.profile);
-      });
-  }, [router]);
-  return user;
+    if (data && !data.ok) {
+      router.replace("/enter");
+    }
+  }, [data, router]);
+  return { user: data?.profile, isLoading: !data && !error };
 }
