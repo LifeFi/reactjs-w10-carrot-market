be23fa0180b60ffe6eb61fa8a1550864d824bfb7
topcircle <topcircle@gmail.com>
2023-02-22 15:30:48 +0900

    12.4 answer

---
+++ pages/api/posts/[id]/answers.ts
@@ -0,0 +1,52 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+    session: { user },
+    body: { answer },
+  } = req;
+
+  const post = await client.post.findUnique({
+    where: {
+      id: Number(id?.toString()),
+    },
+    select: {
+      id: true,
+    },
+  });
+  const newAnswer = await client.answer.create({
+    data: {
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+      post: {
+        connect: {
+          id: Number(id?.toString()),
+        },
+      },
+      answer,
+    },
+  });
+  console.log(newAnswer);
+  res.json({
+    ok: true,
+    answer: newAnswer,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["POST"],
+    fn: handler,
+  })
+);
---
+++ pages/api/posts/[id]/wonder.ts
@@ -0,0 +1,59 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+
+  const alreadyExists = await client.wondering.findFirst({
+    // 찾으려는 필드가 Unique 필드가 아니기 때문에, findFirst 이용.
+    where: {
+      userId: user?.id,
+      postId: Number(id?.toString()),
+    },
+    select: {
+      id: true,
+    },
+  });
+  if (alreadyExists) {
+    await client.wondering.delete({
+      where: {
+        id: alreadyExists.id,
+      },
+    });
+  } else {
+    await client.wondering.create({
+      data: {
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+        post: {
+          connect: {
+            id: Number(id?.toString()),
+          },
+        },
+      },
+    });
+  }
+
+  res.json({
+    ok: true,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["POST"],
+    fn: handler,
+  })
+);
