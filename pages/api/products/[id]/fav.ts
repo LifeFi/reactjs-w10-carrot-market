d49ac85246030190f5e9e8b6f6c344a1c368f459
topcircle <topcircle@gmail.com>
2023-02-22 10:45:49 +0900

    11.8 bound Mutation

--- pages/api/products/[id]/fav.ts
+++ pages/api/products/[id]/fav.ts
@@ -8,13 +8,45 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { id } = req.query;
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+  const alreadyExists = await client.fav.findFirst({
+    // 찾으려는 필드가 Unique 필드가 아니기 때문에, findFirst 이용.
+    where: {
+      productId: Number(id?.toString()),
+      userId: user?.id,
+    },
+  });
+  if (alreadyExists) {
+    await client.fav.delete({
+      where: {
+        id: alreadyExists.id,
+      },
+    });
+  } else {
+    await client.fav.create({
+      data: {
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+        product: {
+          connect: {
+            id: Number(id?.toString()),
+          },
+        },
+      },
+    });
+  }
   res.json({ ok: true });
 }
 
 export default withApiSession(
   withHandler({
-    methods: ["GET"],
+    methods: ["POST"],
     fn: handler,
   })
 );
