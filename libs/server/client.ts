65a4af079d0cd9c13565be7c2160e29653fccabb
topcircle <topcircle@gmail.com>
2023-02-23 00:39:34 +0900

    SQL 로그 확인 기능

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -5,6 +5,7 @@ declare global {
 } // interface 사용도 괜찮다고 함.
 
 const client = global.client || new PrismaClient();
+// const client = global.client || new PrismaClient({ log: ["query"] }); // 콘솔에서, SQL문을 확인할 수 있다.
 
 if (process.env.NODE_ENV === "development") global.client = client;
 
