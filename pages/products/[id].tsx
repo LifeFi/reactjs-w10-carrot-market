6fc0911c7c5c71d8e952ab2b5481353732170291
topcircle <topcircle@gmail.com>
2023-02-23 12:52:13 +0900

    15.8 Product Images

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -43,9 +43,21 @@ const ItemDetail: NextPage = () => {
       ) : (
         <div className="px-4  py-4">
           <div className="mb-8">
-            <div className="h-96 bg-slate-300" />
+            <img
+              src={`https://imagedelivery.net/OBj4-AubflPByVfhX39jxA/${data.product.image}/public`}
+              className="h-96 bg-slate-300"
+            />
+
             <div className="flex cursor-pointer items-center space-x-3 border-t border-b py-3">
-              <div className="h-12 w-12 rounded-full bg-slate-300" />
+              {data.product.user.avatar ? (
+                <img
+                  src={`https://imagedelivery.net/OBj4-AubflPByVfhX39jxA/${data.product.user.avatar}/avatar`}
+                  className="h-12 w-12 rounded-full bg-slate-300"
+                />
+              ) : (
+                <div className="h-12 w-12 rounded-full bg-slate-300" />
+              )}
+
               <div>
                 <p className="text-sm font-medium text-gray-700">
                   {data.product.user.name}
